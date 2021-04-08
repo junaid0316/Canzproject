03c6c1570b6c9af1666ae8a249e31ea3a6392720
Junaid <mj448551@gmail.com>
2021-04-08 13:26:00 +0500

    Canz Project

---
+++ canzapp.js
@@ -0,0 +1,8 @@
+if ($(window).width() < 600) {
+    document.getElementById("drpp").classList.remove("drps");
+    document.getElementById("drpp1").classList.remove("drps");
+    document.getElementById("drpps").classList.remove("drps");
+    document.getElementById("drpp2").classList.remove("drps");
+    document.getElementById("drpp3").classList.remove("drps");
+}
+
